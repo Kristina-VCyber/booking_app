a4fadd5388400b75a0500cc3d7646470f089f47e
Kristina-VCyber <hatkova@inbox.ru>
2023-02-19 22:03:41 +0300

    Add:home icon, Fix: rating container position

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { ReactComponent as Logout } from "./logout.svg";
+export { ReactComponent as Home } from "./home.svg";
+export { ReactComponent as HomeRoof } from "./home_roof.svg";
+export { ReactComponent as Dash } from "./dash.svg";
+
--- src/components/databaseList/DatabaseListItem.tsx
+++ src/components/databaseList/DatabaseListItem.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import styles from "./DatabaseListItem.module.css"
-import { BasicRating} from "./Rating";
+import { BasicRating } from "./Rating";
+import { Dash, Home, HomeRoof } from "../../assets";
 
 export const DatabaseListItem = () => {
     return (
         <div className={styles.itemWrapper}>
             <div className={styles.iconWrapper}>
-                <span/>
+                <HomeRoof className={styles.homeRoofIcon}/>
+                <Home className={styles.homeIcon}/>
             </div>
             <div className={styles.ratingWrapper}>
                 <h3>Moscow Marriott Grand Hotel</h3>
                 <div className={styles.dateWrapper}>
                     <time>07.07.2023</time>
-                    <span/>
-                    <p></p>
+                    <Dash className={styles.dash}/>
+                    <p> 1 день</p>
                 </div>
                 <BasicRating/>
 
