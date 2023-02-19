b52f81180956ff9882495eabc3dcc277510588b1
Kristina-VCyber <hatkova@inbox.ru>
2023-02-19 11:16:13 +0300

    delete: idea folder

--- src/components/HotelsCheck/databaseCard/DatabaseHeader.tsx
+++ src/components/HotelsCheck/databaseCard/DatabaseHeader.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import styles from "./DatabaseCard.module.css";
+import styles from "./DatabaseHeader.module.css";
 
 export const DatabaseHeader = () => {
     return (
         <div className={styles.cardHeaderWrapper}>
          <div className={styles.cardTitleWrapper}>
-            <h1 className={styles.titleWithIcon}>Отели</h1>
+            <h1>Отели</h1>
+             <span className={styles.icon}/>
             <h1>Москва</h1>
          </div>
             <time>18.02.2023</time>
