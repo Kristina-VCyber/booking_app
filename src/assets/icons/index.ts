f58ffed641f62f75c18c30c025518b71d3ada4f6
Kristina-VCyber <hatkova@inbox.ru>
2023-02-22 00:02:01 +0300

    Add: pointer cursor to carousel,and heart img; Add: primary btn hover and active;

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,6 +4,8 @@ export { ReactComponent as HomeRoof } from "./home_roof.svg";
 export { ReactComponent as Dash } from "./dash.svg";
 export { ReactComponent as HeartEmpty } from "./heart.svg";
 export { ReactComponent as HeartRed } from "./heart_red.svg";
+export { ReactComponent as HeartGrey } from "./heart_grey.svg";
+
 export { ReactComponent as Arrow } from "./arrow.svg";
 export { ReactComponent as Baseline } from "./baseline.svg";
 export { ReactComponent as ArrowUp } from "./arrow_up.svg";
